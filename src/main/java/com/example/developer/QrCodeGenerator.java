398ad4cdc3cdc86ccb081585dfb72ae6c33363ef
Umadevi R <umad18323.cs@rmkec.ac.in>
2021-07-15 20:49:40 +0530

    Cleaning Up Code

--- src/main/java/com/example/developer/QrCodeGenerator.java
+++ src/main/java/com/example/developer/QrCodeGenerator.java
@@ -12,26 +12,13 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QrCodeGenerator {
-
-		public static void generateQRCodeImage(String text, int width, int height, String filePath)
-	            throws WriterException, IOException {
-	        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-	        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-	        Path path = FileSystems.getDefault().getPath(filePath);
-	        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-	       
-	    }
-		
-		
-		public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
-		    QRCodeWriter qrCodeWriter = new QRCodeWriter();
-		    BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-		    
-		    ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-		    MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-		    byte[] pngData = pngOutputStream.toByteArray(); 
-		    return pngData;
-		}
-
+	public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException 
+	{
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);    
+		ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+		MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+		byte[] pngData = pngOutputStream.toByteArray(); 
+		return pngData;
+	}
 }
